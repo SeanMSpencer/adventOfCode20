ea149cd101fb75e814c60391d81e1cd99a6743af
Sean.Spencer <seanspencer95@gmail.com>
2022-03-02 17:42:42 -0500

    Part2

--- day6/day6.py
+++ day6/day6.py
@@ -1,3 +1,5 @@
+import string
+
 input_data = "day6_data.txt"
 
 # opens txt file and splits the groups based on two consecutive newlines
@@ -10,3 +12,12 @@ for form in forms:
 
 print("The number of questions answered with yes is: {x}".format(
     x=sum(clean_list)))
+
+
+count = 0
+for form in forms:
+    ind_ans = form.split("\n")
+    for c in string.ascii_lowercase:
+        count += all([c in a for a in ind_ans])
+
+print(f"Part 2: {count}")
